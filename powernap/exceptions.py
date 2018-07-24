2c517cbbdc8157929033c9bc18dbf07c9d7cc032
Harvey Chapman <hchapman@hivelocity.net>
2018-07-23 22:37:59 -0400

    style(exceptions): fix linter errors adding whitespace and doc strings

--- powernap/exceptions.py
+++ powernap/exceptions.py
@@ -1,3 +1,5 @@
+"""Powernap API Errors."""
+
 from powernap.http_codes import (
     error_code,
     not_found_code,
@@ -9,8 +11,10 @@ from powernap.http_codes import (
 
 class ApiError(Exception):
     """Triggers Flask error handler :meth:`core.api.exceptions.api_error`."""
+
     def __init__(self, description=None):
-        """
+        """Create an ApiError with an optional description.
+
         :param description: A json serializable object or data structure
             that is passed to the :meth:`core.api.api_response` with
             the key `errors`.
@@ -21,11 +25,13 @@ class ApiError(Exception):
 
 class InvalidFormError(ApiError):
     """Raise when user does a POST or PUT request with invalid data."""
+
     code = error_code
 
 
 class InvalidJsonError(ApiError):
     """Raise when user does a POST or PUT request with bad JSON."""
+
     code = error_code
 
 
@@ -34,29 +40,35 @@ class InvalidDataFormatError(ApiError):
 
     Mostly used for JSON fields.
     """
+
     code = error_code
 
 
 class OwnerError(ApiError):
     """Raise when user tries to access a db model they don't own."""
+
     code = not_found_code
 
 
 class PermissionError(ApiError):
     """Raise when user tries to access page without proper permissions."""
+
     code = forbidden_code
 
 
 class RequestLimitError(ApiError):
     """Raise when user's session has exceeded the rate limit."""
+
     code = too_many_requests_code
 
 
 class UnauthorizedError(ApiError):
     """Raise when anonymous user tries to access page without api token."""
+
     code = unauthorized_code
 
 
 class UbersmithAPIError(ApiError):
     """Raise when a ResponseError is raised by the ubersmith API."""
+
     code = error_code
